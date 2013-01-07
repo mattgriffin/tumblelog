33344656708196e990d92ff5801375ee4dbba483
mattgriffin <me@mattgriffin.com>
2013-01-07 21:59:40 +0000

    Comments are working.

--- urls.py
+++ urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls.defaults import patterns, include, url
 from django.views.generic import ListView, DetailView
 from tumblelog.models import Post
+from tumblelog.views import PostDetailView
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
@@ -15,13 +16,13 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
     url(r'^$', ListView.as_view(
 	queryset=Post.objects.all(), 
 	context_object_name="posts_list"), 
 	name="home"
     ),
-    url(r'^post/(?P<slug>[a-zA-Z0-9-]+)/$', DetailView.as_view(
+    url(r'^post/(?P<slug>[a-zA-Z0-9-]+)/$', PostDetailView.as_view(
 	queryset=Post.objects.all(), 
 	context_object_name="post"), 
 	name="post"
